e21803c8451775114178cf60d1ca809c758ac29f
Ange1D <47231898+Ange1D@users.noreply.github.com>
2023-06-22 20:55:33 -0600

    S3E1

--- e3/src/main/java/org/bedu/App.java
+++ e3/src/main/java/org/bedu/App.java
@@ -1,4 +1,32 @@
 package org.bedu;
 
+import java.util.Scanner;
+
 public class App {
+
+    public static void main(String[] args) {
+        IRandomNumberGenerator generator = new RandomNumberGenerator();
+        GuessNumberGame game = new GuessNumberGame(generator);
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.println("Adivina un número entre 0 y 100");
+            game.startGame();
+
+            boolean hasWon = false;
+
+            while (!hasWon) {
+                System.out.println("Ingresa tu número:");
+                int number = sc.nextInt();
+
+                hasWon = game.guessNumber(number);
+
+                if (!hasWon) {
+                    System.out.println("Intenta de nuevo...");
+                }
+            }
+        } catch (GameOverException ex) {
+            System.out.println("Perdiste :(");
+        }
+    }
 }
---
+++ e3/src/main/java/org/bedu/GuessNumberGame.java
@@ -0,0 +1,39 @@
+package org.bedu;
+
+public class GuessNumberGame {
+
+    private IRandomNumberGenerator generator;
+    private int secretNumber;
+    private int tries;
+
+    public GuessNumberGame(IRandomNumberGenerator generator) {
+        this.generator = generator;
+    }
+
+    public void startGame() {
+        tries = 0;
+        secretNumber = generator.generateRandomNumber();
+    }
+
+    public int getTries() {
+        return tries;
+    }
+
+    public boolean guessNumber(int number) throws GameOverException, OutOfRangeException {
+        if (number > generator.getMaxRange() || number < generator.getMinRange()) {
+            throw new OutOfRangeException();
+        }
+
+        if (number == secretNumber) {
+            return true;
+        }
+
+        tries++;
+
+        if (tries == 3) {
+            throw new GameOverException(); // GameOver
+        }
+
+        return false;
+    }
+}
