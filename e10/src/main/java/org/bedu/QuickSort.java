a444be165e99fe564810e2938a15175c8a444abb
Ange1D <47231898+Ange1D@users.noreply.github.com>
2023-07-13 22:02:06 -0600

    S8E1

---
+++ e10/src/main/java/org/bedu/QuickSort.java
@@ -0,0 +1,42 @@
+package org.bedu;
+
+public class QuickSort {
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 9, 4, 7, 2, 1, 6, 3, 8, 5 };
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println("Array ordenado: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
